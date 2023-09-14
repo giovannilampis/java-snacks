bde84fe22b843de6afd2fc013f051efeb5c2d047
giovannilampis <giolampis@hotmail.com>
2023-09-14 11:49:04 +0200

    add snack 1 to Exercises class

--- src/snacksJava/Exercises.java
+++ src/snacksJava/Exercises.java
@@ -1,5 +1,27 @@
 package snacksJava;
 
+import java.util.Scanner;
+
 public class Exercises {
 
+	public static void main(String[] args) {
+		
+		// SNACK 1 -> Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Inserire Numero ");
+		
+		int numero = scanner.nextInt();
+		
+		if (numero % 2 == 0) {
+            System.out.println("Il numero inserito è pari: " + numero);
+        } else {
+            System.out.println("Il numero inserito è dispari, il successivo è: " + (numero + 1));
+        }
+        
+        scanner.close();
+		
+		
+	}
 }

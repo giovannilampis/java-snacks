c2a535688c2563d636db4dd28be1ee4e2a4c3a97
giovannilampis <giolampis@hotmail.com>
2023-09-14 12:35:57 +0200

    add snack 4 to Exercises class

--- src/snacksJava/Exercises.java
+++ src/snacksJava/Exercises.java
@@ -45,18 +45,54 @@ public class Exercises {
         	
         // SNACK 3 -> Crea un array di numeri interi e fai la somma di tutti gli elementi  che sono in posizione dispari
 		
-		int[] numeri = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//		int[] numeri = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//
+//        int sommaPosizioniDispari = 0;
+//
+//        for (int i = 0; i < numeri.length; i++) {
+//            if (i % 2 != 0) { // Controlla se l'indice è dispari
+//                sommaPosizioniDispari += numeri[i];
+//            }
+//        }
+//
+//        System.out.println("Array di numeri: " + java.util.Arrays.toString(numeri));
+//        System.out.println("Somma degli elementi in posizione dispari: " + sommaPosizioniDispari);
+		
+		
+		
+		
+		// SNACK 4 -> data in input una stringa, verificare se è palindroma oppure no
+		
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Inserisci una stringa: ");
+        String input = scanner.nextLine();
+        
+        if (isPalindroma(input)) {
+            System.out.println("La stringa è palindroma.");
+        } else {
+            System.out.println("La stringa non è palindroma.");
+        }
+        
+        scanner.close();
+    }
 
-        int sommaPosizioniDispari = 0;
-
-        for (int i = 0; i < numeri.length; i++) {
-            if (i % 2 != 0) { // Controlla se l'indice è dispari
-                sommaPosizioniDispari += numeri[i];
+	    // Metodo per verificare se una stringa è palindroma
+	
+	    private static boolean isPalindroma(String str) {
+	    	
+        // Rimuovi gli spazi e converte la stringa in minuscolo per una verifica senza distinzioni tra maiuscole e minuscole
+	    	
+        str = str.replaceAll("\\s+", "").toLowerCase();
+        
+        int lunghezza = str.length();
+        
+        for (int i = 0; i < lunghezza / 2; i++) {
+            if (str.charAt(i) != str.charAt(lunghezza - 1 - i)) {
+                return false;
             }
         }
-
-        System.out.println("Array di numeri: " + java.util.Arrays.toString(numeri));
-        System.out.println("Somma degli elementi in posizione dispari: " + sommaPosizioniDispari);
+        
+        return true;
 
 	}
 }

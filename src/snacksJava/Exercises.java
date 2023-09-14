6a30ccbe14d1e08b15f876a4282f7254a4de9c1a
giovannilampis <giolampis@hotmail.com>
2023-09-14 12:50:57 +0200

    add snack 5 to Exercises class

--- src/snacksJava/Exercises.java
+++ src/snacksJava/Exercises.java
@@ -6,6 +6,9 @@ public class Exercises {
 
 	public static void main(String[] args) {
 		
+		
+		
+		
 		// SNACK 1 -> Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo
 		
 		// Scanner scanner = new Scanner(System.in);
@@ -23,6 +26,7 @@ public class Exercises {
         // scanner.close();
             
             
+		
             
             
         // SNACK 2 -> Generatore di “nomi cognomi” casuali: il Grande Gatsby ha  una lista di nomi e una lista di cognomi, 
@@ -63,36 +67,75 @@ public class Exercises {
 		
 		// SNACK 4 -> data in input una stringa, verificare se è palindroma oppure no
 		
-		Scanner scanner = new Scanner(System.in);
-        System.out.print("Inserisci una stringa: ");
-        String input = scanner.nextLine();
-        
-        if (isPalindroma(input)) {
-            System.out.println("La stringa è palindroma.");
-        } else {
-            System.out.println("La stringa non è palindroma.");
-        }
-        
-        scanner.close();
-    }
+//		Scanner scanner = new Scanner(System.in);
+//        System.out.print("Inserisci una stringa: ");
+//        String input = scanner.nextLine();
+//        
+//        if (isPalindroma(input)) {
+//            System.out.println("La stringa è palindroma.");
+//        } else {
+//            System.out.println("La stringa non è palindroma.");
+//        }
+//        
+//        scanner.close();
+//        
+//    	}
 
 	    // Metodo per verificare se una stringa è palindroma
 	
-	    private static boolean isPalindroma(String str) {
+	    // private static boolean isPalindroma(String str) {
 	    	
         // Rimuovi gli spazi e converte la stringa in minuscolo per una verifica senza distinzioni tra maiuscole e minuscole
 	    	
-        str = str.replaceAll("\\s+", "").toLowerCase();
+//        str = str.replaceAll("\\s+", "").toLowerCase();
+//        
+//        int lunghezza = str.length();
+//        
+//        for (int i = 0; i < lunghezza / 2; i++) {
+//            if (str.charAt(i) != str.charAt(lunghezza - 1 - i)) {
+//                return false;
+//            }
+//        }
+//        
+//        return true;
+		
+		
+		
+		
+		
+		// SNACK 5 ->  Data una stringa in input, mostrare a video quanti caratteri alfabetici contiene, quanti numeri e quanti simboli non alfanumerici.
+		
+        Scanner scanner = new Scanner(System.in);
         
-        int lunghezza = str.length();
-        
-        for (int i = 0; i < lunghezza / 2; i++) {
-            if (str.charAt(i) != str.charAt(lunghezza - 1 - i)) {
-                return false;
+        while (true) {
+            System.out.print("Inserisci una stringa (o 0 per uscire): ");
+            String input = scanner.nextLine();
+            
+            if (input.equals("0")) {
+                // Esci dal ciclo se l'utente inserisce "0"
+            	break; 
             }
+            
+            int caratteriAlfabetici = 0;
+            int numeri = 0;
+            int simboliNonAlfanumerici = 0;
+            
+            for (char carattere : input.toCharArray()) {
+                if (Character.isLetter(carattere)) {
+                    caratteriAlfabetici++;
+                } else if (Character.isDigit(carattere)) {
+                    numeri++;
+                } else {
+                    simboliNonAlfanumerici++;
+                }
+            }
+            
+            System.out.println("Caratteri alfabetici: " + caratteriAlfabetici);
+            System.out.println("Numeri: " + numeri);
+            System.out.println("Simboli non alfanumerici: " + simboliNonAlfanumerici);
         }
         
-        return true;
+        scanner.close();
 
 	}
 }
